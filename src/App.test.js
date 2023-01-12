4a0f1b768f50325b2f03469192b93fc04336a4d9
Veny Cordova <venycordova@gmail.com>
2023-01-11 21:13:38 -0500

    adding jest test mock

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,14 @@
+import {render, screen, waitFor } from '@testing-library/react'
+import App from './App'
 
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'coder'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('coder')
+    })
 
-
+beforeEach(()=> {
+    //sets everything back to inital state before each test
+    fetch.resetMocks();
+})
